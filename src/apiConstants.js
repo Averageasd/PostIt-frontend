0bf66559cb9b4745993329598e7e1cc6568916c5
Averageasd <nhuynh9497@gmail.com>
2024-03-09 15:12:00 -0500

    assigned a env variable to a vartiable and export it

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -1,6 +1,3 @@
-import env from "react-dotenv";
-import dotenv from "dotenv";
-dotenv.config().parsed;
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export {baseUrl};
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import{loadEnv} from "vite";
+import dotenv from 'cross-env';
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
         plugins: [react()],
@@ -17,3 +20,27 @@ export default defineConfig({
         }
     },
 )
+
+// export default defineConfig(({ command, mode }) => {
+//     // Load env file based on `mode` in the current working directory.
+//     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//     const env = loadEnv(mode, process.cwd(), '')
+//     return {
+//         // vite config
+//         plugins: [react()],
+//         define: {
+//             __APP_ENV__: JSON.stringify(env.BASE_URL),
+//         },
+//         build: {
+//             target: "es2022"
+//         },
+//         esbuild: {
+//             target: "es2022"
+//         },
+//         optimizeDeps: {
+//             esbuildOptions: {
+//                 target: "es2022",
+//             }
+//         }
+//     }
+// })
