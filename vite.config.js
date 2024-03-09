64241897d5fe3de578931ee3e24820a3b2410016
Averageasd <nhuynh9497@gmail.com>
2024-03-09 14:00:19 -0500

    updated vite config with builds

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
         plugins: [react()],
-    },
-    {
         build: {
             target: "es2022"
         },
@@ -17,5 +15,5 @@ export default defineConfig({
                 target: "es2022",
             }
         }
-    })
-
+    },
+)
